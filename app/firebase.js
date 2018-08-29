d5e09b2c2fda0cf0bf85495b84f0824f133b761b
bruno-amil@hotmail.com <bruno-amil@hotmail.com>
2018-08-29 17:05:03 -0300

    subindo alteracoes do firebase

---
+++ app/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAIFdXX8Y7QP5hLcICMcGm_sv3nkwaLYVs",
+    authDomain: "carol-admin.firebaseapp.com",
+    databaseURL: "https://carol-admin.firebaseio.com",
+    projectId: "carol-admin",
+    storageBucket: "carol-admin.appspot.com",
+    messagingSenderId: "990152618892"
+  };
+  firebase.initializeApp(config);
